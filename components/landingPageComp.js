04cec90063250b06dd0e88d8374cc0850d699e65
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-10-14 03:44:57 +0800

    adjusted css and html

--- components/landingPageComp.js
+++ components/landingPageComp.js
@@ -6,35 +6,39 @@ const landingPage = () => {
     <div>
       <div className="landing-page">
         <div className="row-container">
-          <div className="category-col-container">
-            <div className="category-header">
-              <label>Basic Information</label>
-            </div>
-            <div className="row-container">
-              <div className="col-container">
-                <button className="card-button">Disease 1</button>
+          <div className="col-container">
+            <div className="category-col-container">
+              <div className="category-header">
+                <label>Basic Information</label>
               </div>
-              <div className="col-container">
-                <button className="card-button">Disease 2</button>
-              </div>
-              <div className="col-container">
-                <button className="card-button">Disease 3</button>
+              <div className="row-container">
+                <div className="col-container">
+                  <button className="card-button">Disease 1</button>
+                </div>
+                <div className="col-container">
+                  <button className="card-button">Disease 2</button>
+                </div>
+                <div className="col-container">
+                  <button className="card-button">Disease 3</button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="category-col-container">
-            <div className="category-header">
-              <label>Families</label>
-            </div>
-            <div className="row-container">
-              <div className="col-container">
-                <button className="card-button">Donate Registry</button>
+          <div className="col-container">
+            <div className="category-col-container">
+              <div className="category-header">
+                <label>Families</label>
               </div>
-              <div className="col-container">
-                <button className="card-button">Support Group</button>
-              </div>
-              <div className="col-container">
-                <button className="card-button">Financial Assistance</button>
+              <div className="row-container">
+                <div className="col-container">
+                  <button className="card-button">Donate Registry</button>
+                </div>
+                <div className="col-container">
+                  <button className="card-button">Support Group</button>
+                </div>
+                <div className="col-container">
+                  <button className="card-button">Financial Assistance</button>
+                </div>
               </div>
             </div>
           </div>
