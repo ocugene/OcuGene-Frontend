d964759d7b59c4cef848bacf0b5d16e5eebfa76d
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-10-13 23:11:59 +0800

    Landing update

--- app/page.js
+++ app/page.js
@@ -1,45 +1,13 @@
 import React from 'react'
 import NavbarLanding from '@/components/navbarLanding'
-import './page.css';
+import LandingComp from '@/components/landingPageComp'
+import Image from 'next/image'
 
-const LandingPage = () => {
+export default function LandingPageComp() {
   return (
     <div>
       <NavbarLanding></NavbarLanding>
-      <div className="landing-page">
-        <div className="row-container">
-          <div className="col-container">
-            <label>Basic Information</label>
-            <div className="row-container">
-              <div className="col-container">
-                <button>Disease 1</button>
-              </div>
-              <div className="col-container">
-                <button>Disease 2</button>
-              </div>
-              <div className="col-container">
-                <button>Disease 3</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-container">
-          <label>Families</label>
-            <div className="row-container">
-              <div className="col-container">
-                <button>Donate Registry</button>
-              </div>
-              <div className="col-container">
-                <button>Support Group</button>
-              </div>
-              <div className="col-container">
-                <button>Financial Assistance</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingComp></LandingComp>
     </div>
   )
 }
-
-export default LandingPage
---
+++ components/landingPageComp.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './landingPage.css';
+
+const landingPage = () => {
+  return (
+    <div>
+      <div className="landing-page">
+        <div className="row-container">
+          <div className="col-container">
+            <label>Basic Information</label>
+            <div className="row-container">
+              <div className="col-container">
+                <button>Disease 1</button>
+              </div>
+              <div className="col-container">
+                <button>Disease 2</button>
+              </div>
+              <div className="col-container">
+                <button>Disease 3</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-container">
+          <label>Families</label>
+            <div className="row-container">
+              <div className="col-container">
+                <button>Donate Registry</button>
+              </div>
+              <div className="col-container">
+                <button>Support Group</button>
+              </div>
+              <div className="col-container">
+                <button>Financial Assistance</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default landingPage
