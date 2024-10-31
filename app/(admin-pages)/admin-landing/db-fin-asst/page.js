fd5f5e71e7ff6d2b94aae4a8e6c363d58ca222f2
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-11-01 01:15:53 +0800

    financial assistance

--- app/(admin-pages)/admin-landing/db-fin-asst/page.js
+++ app/(admin-pages)/admin-landing/db-fin-asst/page.js
@@ -1,8 +1,35 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react';
+import NavbarLanding from '@/components/navbarLanding';
+import SidebarFinAsst from '@/components/finAsstSidebar';
+import New from '@/components/finAsstNew';
+import Done from '@/components/finAsstDone';
+import './finAsst.css'
+
+
 
 const FinancialAssistancePage = () => {
+  const [activeComponent, setActiveComponent] = useState('new');
+
+  const handleButtonClick = (componentName) => {
+    setActiveComponent(componentName);
+  };
+
   return (
-    <div>FinancialAssistancePage</div>
+    <div>
+      <NavbarLanding/>
+      <div className="title-container">
+        <div className='title'>Admin Portal / Financial Assistance</div>
+      </div>
+      <div className="donations-container">
+        <SidebarFinAsst onButtonClick={handleButtonClick} />
+        <div className="content">
+          {activeComponent === 'new' && <New />}
+          {activeComponent === 'done' && <Done />}
+        </div>
+      </div>
+    </div>
   )
 }
 
