e03db4d5ddd0583aee49070f9aad4313beb44147
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-11-01 00:35:41 +0800

    Donation management

--- app/(admin-pages)/admin-landing/db-donation/page.js
+++ app/(admin-pages)/admin-landing/db-donation/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+'use client';
 
-const DonationPage = () => {
+import React, { useState } from 'react';
+import NavbarLanding from '@/components/navbarLanding';
+import SidebarDonations from '@/components/donationsSidebar';
+import New from '@/components/donationsNew';
+import Done from '@/components/donationsDone';
+import './donations.css'
+
+
+
+const SupportActivitiesPage = () => {
+  const [activeComponent, setActiveComponent] = useState('new');
+
+  const handleButtonClick = (componentName) => {
+    setActiveComponent(componentName);
+  };
+
   return (
-    <div>DonationPage</div>
+    <div>
+      <NavbarLanding/>
+      <div className="title-container">
+        <div className='title'>Admin Portal / Registry Donations</div>
+      </div>
+      <div className="donations-container">
+        <SidebarDonations onButtonClick={handleButtonClick} />
+        <div className="content">
+          {activeComponent === 'new' && <New />}
+          {activeComponent === 'done' && <Done />}
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default DonationPage
+export default SupportActivitiesPage
